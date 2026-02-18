de7422a68e1a3438c321b82ac6491a43e637b747
aastiksahu <aastiksahu8@gmail.com>
2026-02-18 17:50:10 +0530

    18-02-2026

--- spring-boot-simple/src/main/java/com/rays/ctl/LoginCtl.java
+++ spring-boot-simple/src/main/java/com/rays/ctl/LoginCtl.java
@@ -14,6 +14,7 @@ import com.rays.common.BaseCtl;
 import com.rays.common.ORSResponse;
 import com.rays.dto.UserDTO;
 import com.rays.form.LoginForm;
+import com.rays.form.UserRegistrationForm;
 import com.rays.service.UserService;
 
 @RestController
@@ -48,6 +49,32 @@ public class LoginCtl extends BaseCtl {
 
 		return res;
 
+	}
+	
+	@PostMapping("signUp")
+	public ORSResponse signUp(@RequestBody @Valid UserRegistrationForm form, BindingResult bindingResult) {
+
+		ORSResponse res = validate(bindingResult);
+
+		if (!res.isSuccess()) {
+			return res;
+		}
+
+		UserDTO dto = new UserDTO();
+
+		dto = (UserDTO) form.getDto();
+
+		/*
+		 * dto.setFirstName(form.getFirstName()); dto.setLastName(form.getLastName());
+		 * dto.setLoginId(form.getLoginId()); dto.setPassword(form.getPassword());
+		 * dto.setDob(form.getDob()); dto.setRoleId(2L);
+		 */
+
+		long pk = service.add(dto);
+
+		res.addData(pk);
+		res.addMessage("user Registered Successfully..!!");
+		return res;
 	}
 
 	@PostMapping("logout")
